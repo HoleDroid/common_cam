predator112/android_device_asus_sdm660-common

sdm660-common: camera: Fix path for in-tree build
Signed-off-by: SayanAcharya <rowdysayan99@gmail.com>
Change-Id: Ic180a59dfa52d81484471e0b6e630fe545cdaef4

camera/QCamera2/Android.mk
@@ -149,7 +149,7 @@ LOCAL_HEADER_LIBRARIES += libhardware_headers
 
 #HAL 1.0 Include paths
 LOCAL_C_INCLUDES += \
-        hardware/qcom/camera/QCamera2/HAL
+        $(COMMON_PATH)/camera/QCamera2/HAL
 
 ifeq ($(TARGET_COMPILE_WITH_MSM_KERNEL),true)
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include

HoleDroid/common_cam

Signed-off-by: pkm774 <mprabhat774@gmail.com>
Signed-off-by: harshhaareddy <harshhaa03@gmail.com>

camera/QCamera2/Android.mk
@@ -153,7 +153,7 @@ LOCAL_HEADER_LIBRARIES += libhardware_headers
 
 #HAL 1.0 Include paths
 LOCAL_C_INCLUDES += \
-        hardware/qcom/camera/QCamera2/HAL
+        $(DEVICE_PATH)/camera/QCamera2/HAL
 
 ifeq ($(TARGET_COMPILE_WITH_MSM_KERNEL),true)
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include